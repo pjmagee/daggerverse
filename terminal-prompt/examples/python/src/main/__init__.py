9f544c2b8c10bf478edf05d9d69669387d587ee0
Rafael Pirolla <spam@pirolla.com>
2025-07-12 18:29:14 +0200

    Update the name of the function from prompt to terminal_prompt

--- terminal-prompt/examples/python/src/main/__init__.py
+++ terminal-prompt/examples/python/src/main/__init__.py
@@ -11,7 +11,7 @@ class Examples:
     @function
     async def prompt_choices(self) -> str:
         """Prompt with choices"""
-        result = dag.prompt().with_choices(["apple", "bear", "orange"]).with_ci(False).execute()  # choices
+        result = dag.terminal_prompt().with_choices(["apple", "bear", "orange"]).with_ci(False).execute()  # choices
         outcome = await result.outcome()  # true or false based on user input
         input = await result.input()  # the user input
         return f"Outcome: {outcome}, Input: {input}"
@@ -19,7 +19,7 @@ class Examples:
     @function
     async def prompt_input(self, user_input: str, ci: bool) -> str:
         """Prompt with yes/no options"""
-        result = dag.prompt().with_msg("Continue? (y/n)").with_input(user_input).with_ci(ci).execute()
+        result = dag.terminal_prompt().with_msg("Continue? (y/n)").with_input(user_input).with_ci(ci).execute()
         outcome = await result.outcome()  # true or false based on user input
         input = await result.input()  # the user input
         return f"Outcome: {outcome}, Input: {input}"
@@ -27,7 +27,7 @@ class Examples:
     @function
     async def prompt_options(self, user_input: str = "y", ci: bool = True) -> str:
         """Prompt with options"""
-        result = dag.prompt().with_options(
+        result = dag.terminal_prompt().with_options(
             ci=ci,  # ci mode, disables terminal prompt
             msg="Continue? (y/n)",  # custom message
             input=user_input,  # input from ci pipeline
